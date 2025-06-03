cac43a2f3c407148684920bdbd65f81375d51c42
amresh1kumar <amreshbkp0111@gmail.com>
2025-06-03 08:21:36 +0530

    update

---
+++ backend/Input_From_command_line/file.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+const dirPath = path.join(__dirname, 'create_dynamic_files')
+// console.log(dirPath);
+
+// for create and write 
+// for(i=0; i<5; i++){
+// fs.writeFileSync(dirPath+`/newfil${i}`+".txt","a simple text file" )
+
+//  fs.writeFileSync(dirPath+"/newfile"+i+".txt","a simple text file" )
+// }
+
+
+//for read
+// fs.readdir(dirPath, (err, create_dynamic_files) => {
+//    create_dynamic_files.forEach((item) => {
+//       console.log(item)
+//    })
+// })
+
+
--- backend/Input_From_command_line/index.js
+++ backend/Input_From_command_line/index.js
@@ -1,22 +1,26 @@
-// its give all property
-console.log(process);
 
+// console.log(process); // its show all property
+// console.log(process.argv); // argument vector
 // console.log(process.argv[2]);
 
-// const fs = require('fs');
+const fs = require('fs'); // import file system
 
+//create file cmd = node index.js apple.txt 'apple'
 // const input = process.argv;
+// fs.writeFileSync(input[2],input[3])
 
-// if(input[2]== 'add')
-// {
-//    fs.writeFileSync(input[3] ,input[4])
-// } else if(input[2]=='remove')
-// {
-//    fs.unlinkSync(input[3])
-// }
-// else{
-//    console.log("inviled input");
-// }
+const input = process.argv;
+
+if (input[2] == 'add') // cmd--> node index.js add add.txt 'add file'
+{
+   fs.writeFileSync(input[3], input[4])
+} else if (input[2] == 'remove')  // -->node index.js remove add.txt
+{
+   fs.unlinkSync(input[3])
+}
+else {
+   console.log("inviled input");
+}
 
 
 
